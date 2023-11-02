183ffad7818d0e46a376393fffaaef87e4b7dd15
Leonardo Casimiro Martinez <lcasimiro@anovo.es>
2023-11-02 12:32:46 +0100

    creo la carpeta pods y meto ahi la rest-api a usar en base a mappers

--- src/dals/house/index.ts
+++ src/dals/house/index.ts
@@ -1,2 +1,2 @@
 export * from "./house.model.js";
-//export * from "./repositories/index.js";
+export * from "./repositories/index.js";
--- src/dals/index.ts
+++ src/dals/index.ts
@@ -1,1 +1,2 @@
 export * from "./book/index.js";
+export * from "./house/index.js";
