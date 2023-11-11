8c261df4049fbc570167dcdaa7bead626812de5b
Leonardo Casimiro Martinez <lcasimiro@anovo.es>
2023-11-11 18:05:58 +0100

    gethouse, savehouse y deletehouse en db-repository

--- src/dals/house/repositories/house.db-repository.ts
+++ src/dals/house/repositories/house.db-repository.ts
@@ -1,20 +1,32 @@
 import {db} from '#core/servers/index.js';
 import { HouseRepository } from "./house.repository.js";
 import { House } from "../house.model.js";
+import {ObjectId} from "mongodb";
 
 export const dbRepository: HouseRepository = {
   
   getHouseList: async (page?: number, pageSize?: number) => {
-    return db.collection<House>("houses").find().toArray();
+    const skip = Boolean(page) ? (page - 1) * pageSize : 0;
+    const limit = (pageSize !== null && pageSize !== undefined && pageSize >= 0) ? pageSize : 0;
+    return await db.collection<House>("houses").find()
+    .skip(skip)
+    .limit(limit)
+    .toArray();
   },
 
   getHouse: async (id: string) => {
     throw new Error("Not implemented");
   },
   saveHouse: async (house: House) => {
-    throw new Error("Not implemented");
+    const { insertedId} = await db.collection('houses').insertOne(house);
+    return {
+      ...house,
+      _id: insertedId
+    }
+
   },
   deleteHouse: async (id: string) => {
-    throw new Error("Not implemented");
+    const { deletedCount } = await db?.collection('houses').deleteOne({_id: new ObjectId(id),});
+    return deletedCount === 1;
   },
 };
