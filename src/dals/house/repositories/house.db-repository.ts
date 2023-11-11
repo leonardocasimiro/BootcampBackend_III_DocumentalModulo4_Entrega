97edc15f80ed5699cb73a5aa8d5f093e09f068f6
Leonardo Casimiro Martinez <lcasimiro@anovo.es>
2023-11-11 17:25:01 +0100

    preparamos el getList en db-repository

--- src/dals/house/repositories/house.db-repository.ts
+++ src/dals/house/repositories/house.db-repository.ts
@@ -1,10 +1,13 @@
+import {db} from '#core/servers/index.js';
 import { HouseRepository } from "./house.repository.js";
 import { House } from "../house.model.js";
 
 export const dbRepository: HouseRepository = {
-  getHouseList: async () => {
-    throw new Error("Not implemented");
+  
+  getHouseList: async (page?: number, pageSize?: number) => {
+    return db.collection<House>("houses").find().toArray();
   },
+
   getHouse: async (id: string) => {
     throw new Error("Not implemented");
   },
